a62af8178434e72dc1de1d93ac65982d2ddd6d20
TNTKongen <138623797+TNTKongen@users.noreply.github.com>
2025-10-20 18:58:56 +0200

    Videos can now be uploaded

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 
 const STATES_STYLES = {0: "btn-danger", 1: "btn-warning", 2: "btn-success"};
 
-let videosContainer = document.querySelector("#videos");
-let newVideoPopup = document.querySelector("#add-video-popup")
-// let btnAddVideo = document.querySelector("#add-video-btn")
+const videosContainer = document.querySelector("#videos");
+const newVideoPopup = document.querySelector("#new-video-popup");
+
+const newVideoForm = document.querySelector("#new-video-form");
+const newVideoFormUrl = document.querySelector("#new-video-url");
+const newVideoFormTitle = document.querySelector("#new-video-title");
+const newVideoFormNotes = document.querySelector("#new-video-notes");
+const newVideoFormState = document.querySelector("#new-video-state");
+
 let videos = [];
 
-function addVideo(index) {
+function addVideoHTML(index) {
     let video_data = videos[index];
     const statusCSS = STATES_STYLES[video_data["state"]] + " btn video";
 
@@ -22,29 +28,61 @@ function addVideo(index) {
 };
 
 async function refreshVideos(query=false) {
-    videosContainer.innerHTML = ""
+    videosContainer.innerHTML = "";
     if (query) {
-        videos = await getVideos()
+        videos = await getVideos();
     } else {
-        videos = JSON.parse(sessionStorage.getItem("videos_cache"))
+        videos = JSON.parse(sessionStorage.getItem("videos_cache"));
         if (videos === null) {
-            videos = await getVideos()
-        }
-    }
+            videos = await getVideos();
+        };
+    };
     
     for (let i = 0; i < videos.length; i++) {
-        addVideo(i)
-    }
-    sessionStorage.setItem("videos_cache", JSON.stringify(videos))
-}
+        addVideoHTML(i);
+    };
+    sessionStorage.setItem("videos_cache", JSON.stringify(videos));
+};
 
 function setOpenPopup(popupId, setOpen) {
     let popup = document.getElementById(popupId);
     if (setOpen) {
-        popup.classList.remove("closed-popup")
+        popup.classList.remove("closed-popup");
     } else {
-        popup.classList.add("closed-popup")
+        popup.classList.add("closed-popup");
+    };
+};
+
+newVideoForm.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target)
+
+    setOpenPopup("new-video-popup", false);
+    const data = await insertNewVideo(
+        formData.get("new-video-url"),
+        formData.get("new-video-state") == "3",
+        formData.get("new-video-title"),
+        formData.get("new-video-notes"),
+        formData.get("new-video-state")
+    );
+    if (data == {}) {
+        alert("Failed to add video (if you're seeing this, please tell me on signal)")
+        setOpenPopup("new-video-popup", true);
+        return
     }
-}
+    addVideoDataToVideos(data)
+    refreshVideos()
+    newVideoForm.reset();
+});
+
+function addVideoDataToVideos(data) {
+    for (let i=0; i<videos.length; i++) {
+        const video = videos[i];
+        if (video.id == data.id) {
+            videos[i] = Object.assign(video, data)
+            break
+        };
+    };
+};
 
 refreshVideos(false);
--- supabase.js
+++ supabase.js
@@ -2,19 +2,18 @@
 const supabaseUrl = "https://ogepwndefxyzwxeuvegs.supabase.co";
 const supabaseKey = "sb_publishable_XUKwD25UnuJBB5xwu88oFw_CQXu3whu";
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
-
+const videosSelectData = `
+    id,
+    title,
+    author,
+    state
+`
 
 async function getVideos() {
     const { data, error } = await supabaseClient
-        .from('videos')
-        .select(
-            `
-            title,
-            author,
-            state
-            `,
-        )
-        .order('state', { ascending: true });
+        .from("videos")
+        .select(videosSelectData)
+        .order("state", { ascending: true });
     
     if (error) {
         console.warn(`Failed to get videos: ${error.message}`)
@@ -22,3 +21,14 @@ async function getVideos() {
     }
     return data;
 };
+async function insertNewVideo(url, url_valid, title, notes, state) {
+    const { data, error } = await supabaseClient
+        .from("videos")
+        .insert({url: url, url_valid: url_valid, title: title, notes: notes, state: state})
+        .select(videosSelectData)
+    if (error) {
+        console.warn(`Failed to insert video: ${error.message}`)
+        return {}
+    }
+    return data.data
+}
